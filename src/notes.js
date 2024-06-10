16bf071c83c0fa1d256f09b39d2b1dfde134335c
Kalash <kalash.s@ahduni.edu.in>
2024-06-10 17:57:17 +0530

    Add notes component

---
+++ src/notes.js
@@ -0,0 +1,15 @@
+
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "Delegation is delegation"
+    },
+    {
+        key: 2,
+        title: "Loops",
+        content: "Hoe to keep a programmer in the loop forever!"
+    }
+]
+
+export default notes;
