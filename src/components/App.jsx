c79704b01595c89650d42c17b8d5260f04259f66
Kalash <kalash.s@ahduni.edu.in>
2024-06-15 18:10:56 +0530

    Implement Add/Remove functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "./Header";
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
-
+// import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newnote) {
+        console.log(newnote);
+        setNotes(prevNotes => {
+            return [...prevNotes, newnote];
+        })
+    }
+
+    function deleteNote(id) {
+        console.log("Delete is triggered");
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(note => {
+            <CreateArea
+                onAdd={addNote}
+            />
+            {notes.map((noteitem, index) => {
                 return <Note
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />;
+                    key={index}
+                    id={index}
+                    title={noteitem.title} content={noteitem.content} onDelete={deleteNote}
+                />
             })}
             <Footer />
         </div>
